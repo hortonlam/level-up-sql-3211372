e1674e0b79fb2e2312b730f7a1fdc8918343c88f
hortonlam <118437348+hortonlam@users.noreply.github.com>
2022-11-19 21:43:37 +0800

    Update 01_01_invitations_ch.sql

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,5 +1,5 @@
 -- Generate a list of customer information.
-select * from invitation 
+select * from invit 
 
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
